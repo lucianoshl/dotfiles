0e5b856142d583daa61f3a56010c01c93d4fdf7c
Luciano Lopes <lucianoshl@gmail.com>
2019-04-17 15:07:15 -0300

    doltfiles

--- dotfiles/nativefier/whatsapp.js
+++ dotfiles/nativefier/whatsapp.js
@@ -1,2 +1,1 @@
-if (document.body.innerText.replace(/\n/g, ' ').search(/whatsapp works with.*to use whatsapp.*update/i) === 0)
-	navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
+if (document.body.innerText.replace(/\n/g, ' ').search(/whatsapp works with.*to use whatsapp.*update/i) !== -1) navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
